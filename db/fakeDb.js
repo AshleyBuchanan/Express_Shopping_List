f4bde60432dca1665fd31c3a2ee59b4be94428d3
Ashley Buchanan <ashleyshanebuchanan@gmail.com>
2026-04-04 11:01:08 -0700

    added a more permanent db solution. cleaned up fakeDb code.

--- db/fakeDb.js
+++ db/fakeDb.js
@@ -2,88 +2,89 @@ const fs = require('fs/promises');
 const path = require('path');
 
 const LOG_FILE = path.join(__dirname, 'db_log.jsonl');
+const FDB_FILE = path.join(__dirname, 'fakeDb.json');
 
 class FakeDb {
     constructor() {
-        this.items = [
-        {
-            name: 'popsicle',
-            price: 1.45,
-        },
-        {
-            name: 'cheerios',
-            price: 3.40,
-        },
-        ];
+        this.items = [];
+        this.initialized = false;
+    };
+
+    async init() {
+
+        try {
+            const data = await fs.readFile(FDB_FILE, 'utf8');
+            this.items = JSON.parse(data);
+        } catch (err) {
+            this.items = [];
+            await this.persist();
+            this.saveToLog('createdDB','init');
+        };
+
+        this.initialized = true;
     };
 
     async getAll() {
+        await this.init();
         if (this.items.length === 0) return {message: 'no items stored'};
 
-        await this.saveToLog({
-            op: 'getAll',
-            name: 'all items',
-            timeStamp: new Date().toISOString()
-        });
-
+        await this.persist();
+        await this.saveToLog('getAll', 'all items');
         return this.items;
     };
 
     async get(name) {
+        await this.init();
         const item = this.items.find(item => item.name === name);
         if (!item) return {message: 'no item by that name'};
 
-        await this.saveToLog({
-            op: 'getOne',
-            name: name,
-            timeStamp: new Date().toISOString()
-        });
-
+        await this.persist();
+        await this.saveToLog('getOne', name);
         return item;
     };
 
     async create(item) {
-        await this.saveToLog({
-            op: 'createOne',
-            name: item.name,
-            timeStamp: new Date().toISOString()
-        });
+        await this.init();
 
         this.items.push(item);
+        await this.persist();
+        await this.saveToLog('createOne', item.name);
         return item;
     };
 
     async update(name, updatedItem) {
+        await this.init();
         const item = this.get(name);
-    
         if (!item) return {message: 'no item by that name'};
         if (updatedItem.name !== undefined) item.name = updatedItem.name;
         if (updatedItem.price !== undefined) item.price = updatedItem.price;
 
-        await this.saveToLog({
-            op: 'updateOne',
-            name: name,
-            timeStamp: new Date().toISOString()
-        });
-
+        await this.persist();
+        await this.saveToLog('updateOne', name);
         return item;
     };
 
     async delete(name) {
+        await this.init();
         const index = this.items.findIndex(item => item.name === name);
-
         if (index === -1) return {message: 'no item by that name'};
 
-        await this.saveToLog({
-            op: 'deleteOne',
-            name: name,
-            timeStamp: new Date().toISOString()
-        });
+        await this.persist();
+        await this.saveToLog('deleteOne', name);
         return this.items.splice(index, 1)[0];
     };
 
-    saveToLog = async (record) => {
+    async saveToLog(op, name) {
+        const record = {
+            op: op,
+            name: name,
+            timeStamp: new Date().toISOString()
+        };
         await fs.appendFile(LOG_FILE, `${JSON.stringify(record)}\n`, 'utf8');
+    };
+
+    async persist() {
+        await fs.writeFile(FDB_FILE, JSON.stringify(this.items, null, 2), 'utf8')
     };
 };
 
@@ -91,4 +92,5 @@ module.exports = new FakeDb();
 
 
 // i only did this to demonstrate a single point of access.
-// and to prevent the fakeDb from resetting.
+// and to prevent the fakeDb from resetting... it was annoying.
+// added a log file for db mutation as well.
